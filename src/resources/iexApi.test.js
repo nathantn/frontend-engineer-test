5da4b98095c4db3caa9245d243c6a567ebb79ec2
Nathan Tavares Nascimento <nathan.tnascimento@gmail.com>
2018-07-27 22:45:33 -0300

    refactor: Altera nome do resourcer que fará consultas da api

--- src/resources/iexApi.test.js
+++ src/resources/iexApi.test.js
@@ -1,20 +1,20 @@
-import API from './iextranding';
+import API from './iexApi';
 
-describe('Resource: IEXTranding', () => {
-  test('Objeto implementa fetchStocks', () => {
+describe('resource: IEXTranding', () => {
+  test('object implements fetchStocks', () => {
     expect(API.fetchStocks).toBeTruthy();
   });
 
-  test('Objeto implementa getStock', () => {
+  test('object implements getStock', () => {
     expect(API.fetchStocks).toBeTruthy();
   });
 
-  test('fetchStocks é uma função que retorna uma Promise', () => {
+  test('fetchStocks is a function and return a Promise', () => {
     expect(typeof API.fetchStocks).toBe('function');
     expect(API.fetchStocks()).resolves.toBeTruthy();
   });
 
-  test('getStock é uma função que retorna uma Promise', () => {
+  test('getStock is a function and return a Promise', () => {
     expect(typeof API.getStock).toBe('function');
     expect(API.getStock()).resolves.toBeTruthy();
   });
