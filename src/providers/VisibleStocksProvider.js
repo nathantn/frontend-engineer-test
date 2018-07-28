f0ecdd0f4afcc691654c9ffffc9e74a3bfa53e3b
Nathan Tavares Nascimento <nathan.tnascimento@gmail.com>
2018-07-28 12:04:32 -0300

    feat: Adiciona dependência do react-router-dom

--- src/providers/VisibleStocksProvider.js
+++ src/providers/VisibleStocksProvider.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { getVisibleStocks } from '../selectors/stocks';
+import { fetchStocksIfNeeded } from '../actions/stocks';
 
 const mapStateToProps = state => ({
   visibleStocks: getVisibleStocks(state),
 });
 
+const mapDispatchToProps = dispatch => ({
+  fetchStocksIfNeeded: () => {
+    dispatch(fetchStocksIfNeeded());
+  }
+});
+
 class VisibleStocksProvider extends React.Component {
+  componentDidMount() {
+    this.props.fetchStocksIfNeeded();
+  }
+
   render() {
     return this.props.children(this.props.visibleStocks);
   }
 };
 
-export default connect(mapStateToProps)(VisibleStocksProvider);
+export default connect(mapStateToProps, mapDispatchToProps)(VisibleStocksProvider);
