4213238dcdaed6069d2c36b153046553c83450a4
Nathan Tavares Nascimento <nathan.tnascimento@gmail.com>
2018-07-28 10:15:37 -0300

    feat: Implementa selector dos stocks

--- src/selectors/stocks.js
+++ src/selectors/stocks.js
@@ -1,1 +1,20 @@
-export const visibleStocks = () => {};
+import { createSelector } from 'reselect'
+
+const getStocks = state => state.stocks.data;
+const getStockFilterQuery = state => state.stockFilterQuery;
+
+const filterTopTwentyStocks = list => list.slice(0, 20);
+
+const stockSymbolChecker = (query = '') => stock => stock.symbol.indexOf(query.toUpperCase()) > -1;
+const filterStocksBySymbol = (list, query) => list.filter(stockSymbolChecker(query));
+
+export const getVisibleStocks = createSelector(
+  [getStocks, getStockFilterQuery],
+  (stocks, query) => {
+    if (query) {
+      return filterStocksBySymbol(stocks, query);
+    }
+
+    return filterTopTwentyStocks(stocks);
+  }
+);
