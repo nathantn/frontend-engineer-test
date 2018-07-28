245f60803fc78f52386bbf5fea90e61092d4b7e0
Nathan Tavares Nascimento <nathan.tnascimento@gmail.com>
2018-07-28 10:15:26 -0300

    test: Cria testes para requisito dos selectors dos 'stocks'

--- src/selectors/stocks.test.js
+++ src/selectors/stocks.test.js
@@ -1,14 +1,16 @@
 import { Map, List } from 'immutable';
 
-import { visibleStocks } from './stocks';
+import { getVisibleStocks } from './stocks';
 
+// Data retrived from https://api.iextrading.com/1.0/tops?filter=symbol,lastSalePrice
+// 2018-07-28
 const exampleData = [{'symbol':'KWR','lastSalePrice':161.3},{'symbol':'MHNC','lastSalePrice':23.62},{'symbol':'DWTR','lastSalePrice':0},{'symbol':'TACOW','lastSalePrice':0},{'symbol':'INFU','lastSalePrice':0},{'symbol':'AI','lastSalePrice':10.415},{'symbol':'NLS','lastSalePrice':14},{'symbol':'NSU','lastSalePrice':3.7},{'symbol':'VYMI','lastSalePrice':0},{'symbol':'EPE','lastSalePrice':2.085},{'symbol':'FCFS','lastSalePrice':82.2},{'symbol':'ATU','lastSalePrice':27.925},{'symbol':'STK','lastSalePrice':21.96},{'symbol':'DIT','lastSalePrice':0},{'symbol':'EIM','lastSalePrice':11.765},{'symbol':'EIDX','lastSalePrice':18.37},{'symbol':'DBX','lastSalePrice':30.045},{'symbol':'EZT','lastSalePrice':0},{'symbol':'CVM','lastSalePrice':0.8902},{'symbol':'MMS','lastSalePrice':64.74},{'symbol':'HTD','lastSalePrice':23.23},{'symbol':'DFE','lastSalePrice':0},{'symbol':'PEG','lastSalePrice':51.425},{'symbol':'FPXI','lastSalePrice':0},{'symbol':'FI','lastSalePrice':8.105},{'symbol':'SYLD','lastSalePrice':0},{'symbol':'RLH','lastSalePrice':12.325},{'symbol':'SMIN','lastSalePrice':43.34},{'symbol':'GAMR','lastSalePrice':0}];
 
 describe('selectors: stocks', () => {
-  test('visibleStocks return 20 must important stocks from state when filter is null', () => {
+  test('getVisibleStocks return 20 must important stocks from state when filter is null', () => {
     const state = {
-      stocksFilter: null,
-      stock: {
+      stockFilterQuery: null,
+      stocks: {
         isFething: false,
         data: List(exampleData)
       }
@@ -36,6 +38,24 @@ describe('selectors: stocks', () => {
       { symbol: 'MMS', lastSalePrice: 64.74 }
     ]);
 
-    expect(visibleStocks(state)).toEqual(expected);
+    expect(getVisibleStocks(state)).toEqual(expected);
+  });
+
+  test('getVisibleStocks filter stocks by symbol when stockFilterQuery has a value', () => {
+    const state = {
+      stockFilterQuery: 'A',
+      stocks: {
+        isFething: false,
+        data: List(exampleData)
+      }
+    };
+    const expected = List([
+      {symbol: 'TACOW', lastSalePrice: 0},
+      {symbol: 'AI', lastSalePrice: 10.415},
+      {symbol: 'ATU', lastSalePrice: 27.925},
+      {symbol: 'GAMR', lastSalePrice: 0},
+    ]);
+
+    expect(getVisibleStocks(state)).toEqual(expected);
   });
 });
