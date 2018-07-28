e2447fcff5009263b530149f40c23c149ad85849
Nathan Tavares Nascimento <nathan.tnascimento@gmail.com>
2018-07-27 22:46:39 -0300

    test: Implementa testes requisitos relacionados às actions dos 'stocks'

---
+++ src/actions/stocks.test.js
@@ -0,0 +1,76 @@
+import {
+  fetchStocks,
+  fetchStocksIfNeeded,
+  requestStocks,
+  requestStocksSuccess,
+  requestStocksError,
+} from './stocks';
+import iexApi from '../resources/iexApi'
+
+// Mock functions
+const dispatchMock = jest.fn();
+const getStateMock = jest.fn();
+const fetchStocksMock = jest.spyOn(iexApi, 'fetchStocks');
+
+describe('actions: stocks', () => {
+  beforeEach(() => {
+    dispatchMock.mockReset();
+    fetchStocksMock.mockReset();
+  });
+
+  test('fetchStocks dispatch requestStock action before start the request', () => {
+    fetchStocks()(dispatchMock, getStateMock, { iexApi });
+
+    expect(dispatchMock).toHaveBeenCalledWith(requestStocks());
+    expect(iexApi.fetchStocks).toHaveBeenCalled();
+  });
+
+  test('fetchStocks dispatch requestStockSuccess when request have been done without error', async () => {
+    const data = [1, 2, 3];
+    iexApi.fetchStocks.mockResolvedValue({ ok: true, data });
+
+    await fetchStocks()(dispatchMock, getStateMock, { iexApi });
+
+    expect(dispatchMock).toHaveBeenCalledWith(requestStocks());
+    expect(iexApi.fetchStocks).toHaveBeenCalled();
+    expect(dispatchMock).toHaveBeenLastCalledWith(requestStocksSuccess(data));
+  });
+
+  test('fetchStocks dispatch requestStockError when request fail', async () => {
+    const data = new Error();
+    iexApi.fetchStocks.mockResolvedValue({ ok: false, data });
+
+    await fetchStocks()(dispatchMock, getStateMock, { iexApi });
+
+    expect(dispatchMock).toHaveBeenCalledWith(requestStocks());
+    expect(iexApi.fetchStocks).toHaveBeenCalled();
+    expect(dispatchMock).toHaveBeenLastCalledWith(requestStocksError(data));
+  });
+
+  test('fetchStocksIfNeeded should not dispatch fetchStocks action if isFetching attribute from stocks state is true', () => {
+    getStateMock.mockReturnValue({ stocks: { isFetching: true }});
+
+    fetchStocksIfNeeded()(dispatchMock, getStateMock, { iexApi });
+
+    expect(getStateMock).toHaveBeenCalled();
+    expect(dispatchMock).not.toHaveBeenCalled();
+  });
+
+  test('fetchStocksIfNeeded should not dispatch fetchStocks action if data attribute from stocks state have some value', () => {
+    getStateMock.mockReturnValue({ stocks: { data: [] }});
+
+    fetchStocksIfNeeded()(dispatchMock, getStateMock, { iexApi });
+
+    expect(getStateMock).toHaveBeenCalled();
+    expect(dispatchMock).not.toHaveBeenCalled();
+  });
+
+  test('fetchStocksIfNeeded should dispatch fetchStocks when isFetching and data attribute from stocks states have falsy value', () => {
+    getStateMock.mockReturnValue({ stocks: { isFetching: false, data: null }});
+
+    fetchStocksIfNeeded()(dispatchMock, getStateMock, { iexApi });
+
+    expect(getStateMock).toHaveBeenCalled();
+    expect(dispatchMock).toHaveBeenCalled();
+  });
+});
