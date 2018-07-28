713cb0695e9086a275667e38631d9ed3bbba35b3
Nathan Tavares Nascimento <nathan.tnascimento@gmail.com>
2018-07-28 11:21:53 -0300

    test: Cria teste para definir comportamento do provider com stocks visíveis

---
+++ src/providers/VisibleStocksProvider.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Map } from 'immutable';
+import { Provider } from 'react-redux';
+
+import VisibleStocksProvider from './VisibleStocksProvider';
+import configureStore from '../store/configureStore';
+
+// Use this mocked child to test provider render props
+const childMock = jest.fn().mockReturnValue(<p>{'test'}</p>);
+
+describe('<VisibleStocksProvider />', () => {
+  beforeEach(() => {
+    childMock.mockClear();
+  });
+
+  test('provider calls children with visible stocks state', () => {
+    const div = document.createElement('div');
+    // Initialize store with default value
+    const store = configureStore();
+
+    ReactDOM.render(
+      <Provider store={store}>
+        <VisibleStocksProvider>{childMock}</VisibleStocksProvider>
+      </Provider>,
+      div
+    );
+
+    const expected = Map({
+      isFetching: false,
+      data: null,
+      error: null,
+    });
+    expect(childMock).toHaveBeenCalledWith(expected);
+
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
