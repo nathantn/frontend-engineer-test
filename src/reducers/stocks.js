0044ef8be6c993790195199559ee90d4d3c130c4
Nathan Tavares Nascimento <nathan.tnascimento@gmail.com>
2018-07-27 23:05:17 -0300

    test: Cria testes de requisito para reducer 'stocks'

---
+++ src/reducers/stocks.js
@@ -0,0 +1,35 @@
+import { Map, List } from 'immutable';
+import { REQUEST_STOCKS, REQUEST_STOCKS_SUCCESS, REQUEST_STOCKS_ERROR } from '../actions/stocks';
+
+const defaultState = Map({
+  isFetching: false,
+  data: null,
+  error: null,
+});
+
+const reducer = (state = defaultState, { type, payload }) => {
+  switch (type) {
+    case REQUEST_STOCKS:
+      return Map({
+        isFetching: true,
+        data: null,
+        error: null,
+      });
+    case REQUEST_STOCKS_SUCCESS:
+      return Map({
+        isFetching: false,
+        data: payload,
+        error: null,
+      });
+    case REQUEST_STOCKS_ERROR:
+      return Map({
+        isFetching: false,
+        data: null,
+        error: payload,
+      });
+    default:
+      return state;
+  }
+}
+
+export default reducer;
---
+++ src/reducers/stocks.test.js
@@ -0,0 +1,28 @@
+import { Map, List } from 'immutable';
+
+import { requestStocks, requestStocksSuccess, requestStocksError } from '../actions/stocks';
+import reducer from './stocks';
+
+describe('reducers: stocks', () => {
+  test('reducer starts with default state', () => {
+    const expected = Map({ isFetching: false, data: null, error: null });
+    expect(reducer(undefined, {})).toEqual(expected);
+  });
+
+  test('reducer update isFetching value when receive requestStocks action', () => {
+    const expected = Map({ isFetching: true, data: null, error: null });
+    expect(reducer(undefined, requestStocks())).toEqual(expected);
+  });
+
+  test('reducer update state when receive requestStocksSuccess action', () => {
+    const data = List([1, 2, 3]);
+    const expected = Map({ isFetching: false, data, error: null });
+    expect(reducer(undefined, requestStocksSuccess(data))).toEqual(expected);
+  });
+
+  test('reducer update state when receive requestStocksError action', () => {
+    const error = new Error();
+    const expected = Map({ isFetching: false, data: null, error });
+    expect(reducer(undefined, requestStocksError(error))).toEqual(expected);
+  });
+});
