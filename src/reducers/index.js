c8b0e1caf3494b6fccbb6e6ba4dc7dd1b3aeb013
Nathan Tavares Nascimento <nathan.tnascimento@gmail.com>
2018-07-29 19:20:46 -0300

    feat: Instala plugin para adicionar spread e rest operators na transpilação

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import stockFilterQuery from './stockFilterQuery';
 import stocks from './stocks';
+import stockQuote from './stockQuote';
 
 export default combineReducers({
   stockFilterQuery,
   stocks,
+  stockQuote,
 });
